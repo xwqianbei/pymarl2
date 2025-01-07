2c52fb6791a71e51e02733b138381e20b5a58b1d
xwqianbei <2816739071@qq.com>
2025-01-07 22:15:11 +0800

    initial build llm_select_role pipeline

--- src/modules/agents/__init__.py
+++ src/modules/agents/__init__.py
@@ -9,6 +9,7 @@ from .central_rnn_agent import CentralRNNAgent
 from .mlp_agent import MLPAgent
 from .atten_rnn_agent import ATTRNNAgent
 from .noisy_agents import NoisyRNNAgent
+from .llm_policy_agent import LLMAgent
 
 REGISTRY["rnn"] = RNNAgent
 REGISTRY["n_rnn"] = NRNNAgent
@@ -19,3 +20,4 @@ REGISTRY["central_rnn"] = CentralRNNAgent
 REGISTRY["mlp"] = MLPAgent
 REGISTRY["att_rnn"] = ATTRNNAgent
 REGISTRY["noisy_rnn"] = NoisyRNNAgent
+REGISTRY["llm_policy"] = LLMAgent
