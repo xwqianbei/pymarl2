0108d4f8b3cd4e1b145d83570e9550aa9bd6ac48
熊伟 <ou_3a79b0ff3d713b391ccbe8e40192fc1c@aminer.cn>
2025-03-10 16:58:59 +0800

    fix bug

--- src/modules/agents/llm_agent.py
+++ src/modules/agents/llm_agent.py
@@ -10,12 +10,13 @@ class LLMAgent(nn.Module):
         super(LLMAgent, self).__init__()
         self.args = args
         self.role_embeddings = []
+        self.role_embedding = None
 
         self.fc1 = nn.Linear(input_shape, args.rnn_hidden_dim)
         self.rnn = nn.GRUCell(args.rnn_hidden_dim, args.rnn_hidden_dim)
 
-        self.role_selector = nn.sequential(
-            nn.Linear(args.rnn_hidden, args.role_selector_dim),
+        self.role_selector = nn.Sequential(
+            nn.Linear(args.rnn_hidden_dim, args.role_selector_dim),
             nn.ReLU(),
             nn.Linear(args.role_selector_dim, args.role_num)
         )
@@ -35,31 +36,34 @@ class LLMAgent(nn.Module):
     def init_hidden(self):
         return self.fc1.weight.new(1, self.args.rnn_hidden_dim).zero_()
 
-    def forward(self, inputs, hidden_state):
+    def forward(self, inputs, hidden_state, env_t):
         """
         Args:
             inputs: [bs, a, e]
             hidden_state: [bs, rnn_hidden_dim]
+            env_t: int
         Returns:
             q_val: [bs, a, n_actions]
             hh_out: [bs, a, rnn_hidden_dim]
             role_probs: [bs * a, role_num]        
         """
-        bs, a, e = input.size()
+        bs, a, e = inputs.size()
 
         inputs = inputs.reshape(-1, e) # [bs * a, e]
 
         x = F.relu(self.fc1(inputs))
         h_in = hidden_state.reshape(-1, self.rnn_hidden_dim)
         hh = self.rnn(x, h_in) # [bs * a, rnn_hidden_dim]
+        
 
         role_logits = self.role_selector(hh) # [bs * a, role_num]
         role_probs = F.softmax(role_logits, dim=-1) # [bs * a, role_num]
         role_idx = th.argmax(role_probs, dim=-1)
-        role_embedding = self.role_embeddings[role_idx]
+        if env_t % self.args.role_change_interval == 0:
+            self.role_embedding = self.role_embeddings[role_idx]
 
-        policy_w = self.role_decoder_w(role_embedding).reshape(-1, self.args.rnn_hidden_dim, self.args.n_actions) # [bs * a, rnn_hidden_dim * n_actions]
-        policy_b = self.role_decoder_b(role_embedding) # [bs * a, n_actions]
+        policy_w = self.role_decoder_w(self.role_embedding).reshape(-1, self.args.rnn_hidden_dim, self.args.n_actions) # [bs * a, rnn_hidden_dim * n_actions]
+        policy_b = self.role_decoder_b(self.role_embedding) # [bs * a, n_actions]
 
         q_val = th.bmm(hh.unsqueeze(1), policy_w).squeeze(1) + policy_b # [bs * a, n_actions]
 
